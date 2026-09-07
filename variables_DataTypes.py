5f61674ee3dd737761760a290b5aaa533dabbe67
Mahreen Choudhry <mahreen.ch.3107@gmail.com>
2026-09-07 17:15:54 +0500

    Variables & Data Types

---
+++ variables_DataTypes.py
@@ -0,0 +1,63 @@
+# Print
+
+print("Hello World!")
+
+# Variables
+name = "Mahreen"    #string
+age = 19  #integer
+price = 19.99 #float
+
+# variables name must be simple, short & meaningful.
+
+print(name) 
+print(age)
+print(price)
+
+
+# Data Types using type() function 
+
+print(type(name)) #string
+print(type(age))  #integer
+print(type(price)) #float
+
+
+# Data Types in python
+# 1. Integers
+# 2. String
+# 3. Float
+# 4. Boolean
+# 5. None
+
+
+# Problem => Print Sum
+num1 = 10
+num2 = 20
+sum = num1 + num2
+print("Sum of", num1, "and", num2, "=", sum)
+
+# Expression Execution
+A, B = 2, 3
+Txt = "@"
+
+print(A * Txt * B)  #@@@@@@
+
+# Comments in python
+# This is a single line comment
+
+"""This 
+is a multi-line 
+comment"""
+
+
+# Input in python
+
+name = input("Enter your name: ")
+print("Hello,", name)
+
+#  int input
+age = int(input("Enter your age: "))
+print("You are", age, "years old.")
+
+# float input
+price = float(input("Enter the price: "))
+print("The price is", price)
