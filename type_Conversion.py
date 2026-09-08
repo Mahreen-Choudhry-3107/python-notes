5feb3461f76a2ac903f2e1f9db7b53624a2ac430
Mahreen Choudhry <mahreen.ch.3107@gmail.com>
2026-09-08 08:55:14 +0500

    Type Conversion

---
+++ type_Conversion.py
@@ -0,0 +1,28 @@
+# Type Conversion
+# Type conversion is the process of converting the value of one data type to another data type.
+
+# Implicit Type Conversion
+# Python automatically converts one data type to another data type when necessary.
+
+# Explicit Type Conversion
+# In explicit type conversion, the user manually converts one data type to another data type using built-in functions.
+
+# Example of Implicit Type Conversion
+
+x = 5
+y = 10.0
+z = x + y  # Implicit conversion of x to float
+print(z)
+
+# Output: 15.0
+
+# Example of Explicit Type Conversion
+
+x = "5"
+y = 10
+z = int(x) + y  # Explicit conversion of x to integer
+print(z)
+
+
+
+# Output: 15
