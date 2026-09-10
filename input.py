0098a84c4df9fff4ebcf53997422220a717bcd79
Mahreen Choudhry <mahreen.ch.3107@gmail.com>
2026-09-10 12:05:45 +0500

    input

--- input.py
+++ input.py
@@ -5,3 +5,21 @@ print("Welcome", name, "!")
 
 age = input("Enter your age: ")
 print("You are", age, "years old.")
+
+age_number = int(input("Enter your age as a number: "))
+print("Next year you will be", age_number + 1)
+
+height = float(input("Enter your height in meters: "))
+print("Your height is", height, "meters.")
+
+first_name, last_name = input("Enter your first and last name: ").split()
+print("Your full name is", first_name, last_name)
+
+favorite_color = input("Enter your favorite color: ")
+print("Your favorite color is", favorite_color)
+
+answer = input("Do you like Python? (yes/no): ").lower()
+if answer == "yes":
+	print("Great choice!")
+else:
+	print("You may like it more with practice.")
